599b9d5f7ab96269095d0ae9071a2bcb1ef1c7b8
Pía Ramírez <the.captain312@gmail.com>
2025-09-25 12:47:35 -0600

    Modifico la bd, creo MCT_Validacion para capturar los parametros de la url dinámica con 1 hora de duración

--- app/modules/auth/email_service.py
+++ app/modules/auth/email_service.py
@@ -3,17 +3,21 @@ from email.message import EmailMessage
 from uuid import uuid4
 
 class EmailService:
-    SMTP_HOST = 'smtp.titan.email' #Aquí pongan el smtp que estén usando, me imagino será el de gmail.
+    SMTP_HOST = 'smtp.titan.email'
     SMTP_PORT = 587
-    SMTP_USER = 'agrega el correo de mail'
-    SMTP_PASS = 'pass'
-    FROM_ADDRESS = 'agrega el correo de mail'
+    SMTP_USER = 'aramirez@solucionesatd.com'
+    SMTP_PASS = '17A07n95t%Rmz!'
+    FROM_ADDRESS = 'aramirez@solucionesatd.com'
     FROM_NAME = 'MACTI Proto'
 
     @staticmethod
     def send_validation_email(to_email: str):
+        # Al recuperar los daros del usuario, se debe obtener el instituto al que solicita la cuenta y redirigir al link correspondiente
+        # Remplazar "institute_id_placeholder" por el ID real del instituto
+        institute_id = "institute_id_placeholder"
+
         token = str(uuid4())
-        confirm_link = f"http://localhost:8000/auth/confirm?token={token}"
+        confirm_link = f"http://localhost:3000/{institute_id}/registro/confirmacion?token={token}"
         msg = EmailMessage()
         msg['Subject'] = 'Confirma tu correo'
         msg['From'] = f"{EmailService.FROM_NAME} <{EmailService.FROM_ADDRESS}>"
--- app/modules/auth/models.py
+++ app/modules/auth/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
 from app.core.database import Base
-
+from datetime import datetime
 class User(Base):
     __tablename__ = "users"
 
@@ -26,3 +26,13 @@ class AccountRequest(Base):
     kc_id = Column(String, nullable=True)
     moodle_id = Column(String, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+class MCT_Validacion(Base):
+    __tablename__ = "MCT_Validacion"
+
+    id = Column(Integer, primary_key=True, index=True)
+    correo = Column(String, nullable=False, unique=True, index=True)
+    token = Column(String, nullable=False, unique=True)
+    fecha_solicitud = Column(DateTime, default=datetime.utcnow)
+    fecha_expiracion = Column(DateTime, nullable=True)
+    bandera = Column(Integer, default=0) 
