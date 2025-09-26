2a1ccb9717f00937f44b9adc1f1243562da2b567
Pía Ramirez <the.captain312@gmail.com>
2025-09-25 18:20:50 -0600

    Modificaciones a la bd, para poder ver los registros

--- inspect_bd.py
+++ inspect_bd.py
@@ -1,12 +1,22 @@
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine, inspect, text
 
 engine = create_engine("sqlite:///macti.db")
 inspector = inspect(engine)
 
-print("Tablas en la BD:")
+print("Registros de la bd:")
 print(inspector.get_table_names())
 
 for table in inspector.get_table_names():
-    print(f"\nColumnas de {table}:")
+    print(f"\n Columnas de {table}:")
     for col in inspector.get_columns(table):
-        print(col["name"], col["type"])
+        print(f" - {col['name']} ({col['type']})")
+
+
+    with engine.connect() as conn:
+        result = conn.execute(text(f"SELECT * FROM {table}")).fetchall()
+        print(f"\n Datos en {table}:")
+        if result:
+            for row in result:
+                print(row)
+        else:
+            print(" (sin registros)")
