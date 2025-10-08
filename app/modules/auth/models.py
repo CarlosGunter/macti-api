3d09f23bd9b5fbba796777c3c5e8dcb380a580ff
Pía Ramírez <the.captain312@gmail.com>
2025-10-08 16:24:59 -0600

    Listo enum de estados, según yo el viernes en la reunión se quito

--- app/modules/auth/models.py
+++ app/modules/auth/models.py
@@ -1,32 +1,48 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Enum
 from sqlalchemy.sql import func
 from app.core.database import Base
 from datetime import datetime
-from typing import Optional
-#Cambie unas cosas de la bd así que ejecuten el resetbd.py
+import enum
+
+class AccountStatusEnum(enum.Enum):
+    pending = "pending"
+    approved = "approved"
+    rejected = "rejected"
+    created = "created"
+
+
 class AccountRequest(Base):
     __tablename__ = "account_requests"
-    # TODO: Instituto
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
     email = Column(String, nullable=False, index=True, unique=True)
     course_id = Column(Integer, nullable=False)
-    status = Column(String, default="pending", nullable=False)
+
+    status = Column(
+        Enum(AccountStatusEnum, name="account_status_enum"),
+        default=AccountStatusEnum.pending,
+        nullable=False
+    )
+
     kc_id = Column(String, nullable=True)
     moodle_id = Column(String, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+
     def __repr__(self):
-        return f"<AccountRequest(email='{self.email}', status='{self.status}')>"
-    
+        return f"<AccountRequest(email='{self.email}', status='{self.status.value}')>"
+
+
 class MCT_Validacion(Base):
     __tablename__ = "MCT_Validacion"
+
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, nullable=False, unique=True, index=True)
     token = Column(String, nullable=False, unique=True)
     fecha_solicitud = Column(DateTime, default=datetime.utcnow)
     fecha_expiracion = Column(DateTime, nullable=True)
-    bandera = Column(Integer, default=0) 
+    bandera = Column(Integer, default=0)
+
     def __repr__(self):
         return f"<EmailValidation(email='{self.email}', is_used={self.bandera})>"
