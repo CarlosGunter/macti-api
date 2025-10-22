ef72305975e9e09b79ac603f57fd20bd08360b28
CarlosGunter <carlosgt1702@gmail.com>
2025-10-22 00:46:51 -0600

    fix: he limpiado las rutas, preparando todo para que las respuestas se adecuen a la convención

--- app/modules/auth/routes.py
+++ app/modules/auth/routes.py
@@ -6,15 +6,14 @@ from .controllers.change_status import ChangeStatusController
 from .controllers.create_account import CreateAccountController
 from .schema import AccountRequestSchema, ConfirmAccountSchema, CreateAccountSchema, AccountRequestResponseList
 from app.modules.auth.services.email_service import EmailService
-#pip install python-multipart
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 # Crear solicitud de cuenta
 @router.post("/request-account", summary="Crear una solicitud de cuenta")
 async def request_account(body_info: AccountRequestSchema, db=Depends(get_db)):
-    result = RequestAccountController.request_account(data=body_info, db=db)
-    return {"success": True, "data": result}
+    return RequestAccountController.request_account(data=body_info, db=db)
+
 
 # Listar solicitudes por curso
 @router.get(
@@ -27,22 +26,22 @@ async def list_accounts_requests(
     course_id: int = Query(description="Filtra las solicitudes por ID de curso"),
     db=Depends(get_db)
 ):
-    """Lista todas las solicitudes de cuenta filtradas por curso."""
     return ListAccountRequestsController.list_accounts_requests(db=db, course_id=course_id)
+
 
 # Aprobar o rechazar solicitud y enviar correo
 @router.patch("/confirm-account", summary="Aprobar o rechazar solicitud y enviar correo")
 async def confirm_account(body_info: ConfirmAccountSchema, db=Depends(get_db)):
-    result = await ChangeStatusController.change_status(data=body_info, db=db)
-    return {"success": True, "data": result}
+    return await ChangeStatusController.change_status(data=body_info, db=db)
 
 
 # Crear cuenta en Keycloak y Moodle 
 #es este dónde se recibe la pass nueva para actulizar el key
 @router.post("/create-account", summary="Crear cuenta en Keycloak y Moodle")
 async def create_account(body_info: CreateAccountSchema, db=Depends(get_db)):
-    """Crea una cuenta en Keycloak y Moodle (administrador)."""
     return await CreateAccountController.create_account(data=body_info, db=db)
+
+
 # Confirmar datos token
 @router.get("/confirmacion", summary="Confirmar email con token")
 def confirm_email(token: str):
@@ -50,13 +49,11 @@ def confirm_email(token: str):
 
     if result["success"]:
         return {
-            "success": True,
             "message": result["message"],
             "data": result.get("data")
         }
     else:
         return {
-            "success": False,
             "message": result.get("message", "Error desconocido")
         }
 
