3c6a02f26f96840fde63e32b97c02a782c32965a
Pía Ramírez <the.captain312@gmail.com>
2025-10-27 13:25:48 -0600

    Ajustar las consultas de email por el id, hice un cambio en routes porque me marca un error, pero ya está, elimina el registro cuando se crea el usuario en moodle, ya quedo!

--- app/modules/auth/routes.py
+++ app/modules/auth/routes.py
@@ -45,19 +45,22 @@ async def create_account(body_info: CreateAccountSchema, db=Depends(get_db)):
 # Confirmar datos token
 @router.get("/confirmacion", summary="Confirmar email con token")
 def confirm_email(token: str):
-    result = EmailService.validate_token(token)
-    print("Token:", token)
-    print("Consulta del token:", result)
-
-    if result["success"]:
+    try:
+        result = EmailService.validate_token(token)
+        # Aquí result ya contiene id y email como me lo pediste
         return {
             "success": True,
-            "message": result["message"],
-            "data": result.get("data")
+            "message": "Token válido",
+            "data": result
         }
-    else:
+    except HTTPException as e:
         return {
             "success": False,
-            "message": result.get("message", "Error desconocido")
+            "message": e.detail.get("message", "Error desconocido"),
+            "error_code": e.detail.get("error_code")
         }
-
+    except Exception as e:
+        return {
+            "success": False,
+            "message": str(e)
+        }
