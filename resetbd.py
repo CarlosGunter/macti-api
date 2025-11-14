6b48bd3620fa77e7129fcee82a7265d88c268710
CarlosGunter <carlosgt1702@gmail.com>
2025-11-14 16:48:44 -0600

    feat: se mejoró la gestión de usuarios en Keycloak y se actualizaron las importaciones
    - Se agregó el uso de InstituteEnum en la creación y búsqueda de usuarios en Keycloak.
    - Se eliminó el archivo resetbd.py que ya no era necesario.
    - Se modificó el archivo .gitignore para incluir patrones de archivos SQLite.

--- resetbd.py
+++
@@ -1,9 +0,0 @@
-from app.core.database import Base, engine
-
-# En esta nueva versión me pide instalar lo de abajo para poder resetar la bd e hice unos cambios
-# py -m pip install sqlalchemy
-
-Base.metadata.drop_all(bind=engine)
-Base.metadata.create_all(bind=engine)
-
-print("¡Base de datos reseteada correctamente!")
