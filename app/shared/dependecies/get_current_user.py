b1d1c6c15db9a25cb8e4231753375ad71c5ed9c3
CarlosGunter <carlosgt1702@gmail.com>
2025-12-03 17:44:48 -0600

    feat: la dependecia de autenticación ahora obtiene y retorna el id de moodle.
    - Para usuarios que ya están alojados en la BD, el moodle id se obtiene directamente.
    - Para usuarios que no tienen su registro en la BD se obtiene su ID desde los Web Services y se almacena su información en la BD

--- app/shared/dependecies/get_current_user.py
+++ app/shared/dependecies/get_current_user.py
@@ -1,11 +1,19 @@
+from uuid import UUID
+
 import httpx
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBearer
 from jose import jwt
 from jose.exceptions import ExpiredSignatureError, JWTClaimsError, JWTError
+from pydantic import BaseModel, Field, ValidationError
+from sqlalchemy.exc import MultipleResultsFound, NoResultFound
+from sqlalchemy.orm import Session
 
+from app.core.database import get_db
+from app.modules.auth.models import AccountRequest
 from app.shared.config.kc_configs import keycloak_configs
 from app.shared.enums.institutes_enum import InstitutesEnum
+from app.shared.services.moodle_service import MoodleService
 
 security = HTTPBearer()
 ALGORITHM = "RS256"
@@ -56,21 +64,30 @@ def find_signing_key(jwks: dict, kid: str) -> dict | None:
     return None
 
 
+class CurrentUser(BaseModel):
+    kc_id: UUID = Field(..., alias="sub")
+    email: str = Field(..., alias="email")
+    name: str = Field(..., alias="given_name")
+    last_name: str = Field(..., alias="family_name")
+
+
+class CurrentUserReturn(CurrentUser):
+    moodle_id: int
+
+
 async def get_current_user(
-    institute: InstitutesEnum, credentials=Depends(security)
-) -> dict:
+    institute: InstitutesEnum,
+    db: Session = Depends(get_db),
+    credentials=Depends(security),
+) -> CurrentUserReturn:
     token = credentials.credentials
-    print(f"[DEBUG] Token recibido (primeros 40 chars): {token[:40]}...")
     jwks = await get_jwks_for_institute(institute)
     try:
         unverified_header = jwt.get_unverified_header(token)
-        print(f"[DEBUG] Encabezado del token: {unverified_header}")
 
         kid = unverified_header.get("kid")
-        print(f"[DEBUG] kid extraído: {kid}")
 
         if not kid:
-            print("[ERROR] Token no contiene KID")
             raise HTTPException(
                 status_code=401,
                 detail={
@@ -78,8 +95,8 @@ async def get_current_user(
                     "message": "Falta 'kid' en el encabezado del token",
                 },
             )
+
     except Exception as e:
-        print("[ERROR] Error leyendo encabezado del token:", str(e))
         raise HTTPException(
             status_code=401,
             detail={
@@ -101,9 +118,6 @@ async def get_current_user(
     kc = keycloak_configs[institute]
     issuer = f"{kc.url}/realms/{kc.realm}"
 
-    print(f"[DEBUG] Intentando decodificar token con issuer: {issuer}")
-    print(f"[DEBUG] Usando key type={signing_key.get('kty')} alg={ALGORITHM}")
-
     try:
         payload = jwt.decode(
             token,
@@ -113,13 +127,9 @@ async def get_current_user(
             issuer=issuer,
         )
 
-        print("[DEBUG] Token decodificado correctamente ✔")
-        print(f"[DEBUG] Payload recibido: {payload}")
         azp = payload.get("azp")
-        print(f"[DEBUG] azp: {azp} (debe coincidir con el cliente del frontend)")
 
         if azp != "next-login":
-            print("[ERROR] azp inválido")
             raise HTTPException(
                 status_code=401,
                 detail={
@@ -127,6 +137,15 @@ async def get_current_user(
                     "message": "Cliente (azp) inválido en el token",
                 },
             )
+
+        user_kc_parsed = CurrentUser(**payload)
+        moodle_id = await get_user_moodle_id(user_kc_parsed, db, institute)
+
+        alias_data = user_kc_parsed.model_dump(by_alias=True)
+        alias_data["moodle_id"] = moodle_id
+        user_parsed: CurrentUserReturn = CurrentUserReturn.model_validate(alias_data)
+
+        return user_parsed
 
     except ExpiredSignatureError:
         raise HTTPException(
@@ -149,5 +168,97 @@ async def get_current_user(
             detail={"error_code": "FIRMA_TOKEN_INVALIDA", "message": "Token inválido"},
         ) from e
 
-    print("[DEBUG] Usuario autenticado correctamente")
-    return payload
+    except ValidationError as e:
+        raise HTTPException(
+            status_code=401,
+            detail={
+                "error_code": "VALIDACION_FALLIDA",
+                "message": "Respuesta de información de usuario inválida",
+            },
+        ) from e
+
+
+async def get_user_moodle_id(
+    user_info: CurrentUser, db: Session, institute: InstitutesEnum
+) -> int:
+    """
+    Obtiene el ID de Moodle del usuario a partir de la información del token.
+
+    Verifica en la base de datos u obtiene desde Moodle si es necesario.
+    """
+    kc_id = user_info.kc_id
+    user_email = user_info.email
+
+    try:
+        query = db.query(AccountRequest).filter(AccountRequest.kc_id == kc_id).one()
+        if not query.moodle_id:
+            raise HTTPException(
+                status_code=404,
+                detail={
+                    "error_code": "MOODLE_NO_ENCONTRADO",
+                    "message": "El usuario no tiene un ID de Moodle asociado.",
+                },
+            )
+        return query.moodle_id
+
+    except NoResultFound:
+        moodle_id = await get_moodle_id_from_web_service(
+            institute=institute, user_email=user_email
+        )
+
+        sync_user = AccountRequest(
+            email=user_email,
+            name=user_info.name,
+            last_name=user_info.last_name,
+            institute=institute,
+            moodle_id=moodle_id,
+            kc_id=kc_id,
+        )
+        db.add(sync_user)
+        db.commit()
+        db.refresh(sync_user)
+
+        return moodle_id
+
+    except MultipleResultsFound:
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "error_code": "MULTIPLES_RESULTADOS",
+                "message": "Múltiples usuarios encontrados, error interno.",
+            },
+        ) from MultipleResultsFound
+
+
+async def get_moodle_id_from_web_service(
+    institute: InstitutesEnum, user_email: str
+) -> int:
+    """
+    Sincroniza la información del usuario en la base de datos local
+    obteniendo su ID de Moodle desde el servicio de Moodle.
+    """
+    user_profile_result = await MoodleService.get_user_profile_by_email(
+        institute=institute, user_email=user_email
+    )
+
+    if user_profile_result.error:
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "error_code": "MOODLE_SYNC_ERROR",
+                "message": f"Error al obtener perfil de usuario desde Moodle: {user_profile_result.error}",
+            },
+        )
+
+    user_moodle_id = user_profile_result.user_profile.get("id")
+
+    if not user_moodle_id:
+        raise HTTPException(
+            status_code=404,
+            detail={
+                "error_code": "MOODLE_USER_NOT_FOUND",
+                "message": f"Usuario con email {user_email} no encontrado en Moodle.",
+            },
+        )
+
+    return user_moodle_id
